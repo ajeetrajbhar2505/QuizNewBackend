8c93499097d6580097841e1c12450ee3f859840b
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2025-06-30 23:47:38 +0530

    bypass auth

--- config/socket.js
+++ config/socket.js
@@ -2,6 +2,7 @@ const socketIO = require('socket.io');
 const socketAuth = require('../middlewares/socketAuth');
 const socketService = require('../services/socketService');
 const logger = require('./logger');
+const authController = require('../controllers/authController');
 
 let io;
 
@@ -13,10 +14,44 @@ const initSocket = (server) => {
     },
   });
 
-  io.use(socketAuth.authenticate);
-
+  // Handle unauthenticated connections first
   io.on('connection', (socket) => {
     logger.info(`New client connected: ${socket.id}`);
+    
+    // Register unauthenticated handlers
+    socket.on('auth:register', (data) => authController.register(socket, data));
+    socket.on('auth:login', (data) => authController.login(socket, data));
+    socket.on('auth:google:login', () => authController.googleLogin(socket));
+    socket.on('auth:google:callback', (code) => authController.googleCallback(socket, code));
+    socket.on('auth:facebook:login', () => authController.facebookLogin(socket));
+    socket.on('auth:facebook:callback', (code) => authController.facebookCallback(socket, code));
+    socket.on('auth:otp:send', (email) => authController.sendOTP(socket, email));
+    socket.on('auth:otp:verify', (data) => authController.verifyOTP(socket, data));
+    
+    // Apply authentication middleware for other events
+    socket.use((event, next) => {
+      // List of events that don't require authentication
+      const unauthenticatedEvents = [
+        'auth:register',
+        'auth:login',
+        'auth:google:login',
+        'auth:google:callback',
+        'auth:facebook:login',
+        'auth:facebook:callback',
+        'auth:otp:send',
+        'auth:otp:verify',
+        'disconnect'
+      ];
+      
+      if (unauthenticatedEvents.includes(event[0])) {
+        return next();
+      }
+      
+      // For all other events, use the authentication middleware
+      socketAuth.authenticate(socket, next);
+    });
+
+    // Register the socket with the service
     socketService.registerSocket(socket);
 
     socket.on('disconnect', () => {
