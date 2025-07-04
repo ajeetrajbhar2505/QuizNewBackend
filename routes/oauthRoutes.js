825f91317903f931c897167d3ed8311c536cf85a
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2025-07-04 21:23:31 +0530

    added close timer

--- routes/oauthRoutes.js
+++ routes/oauthRoutes.js
@@ -6,85 +6,93 @@ const { getIO } = require('../config/socket');
 
 // Google OAuth callback
 router.get('/auth/google/callback', async (req, res) => {
-    try {
-        const { code } = req.query;
-        const socket = getIO();
+  try {
+    const { code } = req.query;
+    const socket = getIO();
 
-        if (!socket) {
-            return res.send(`
+    if (!socket) {
+      return res.send(`
           <script>
             window.opener.postMessage({
               type: 'oauth-error',
               message: 'Socket connection not found'
             }, window.opener.location.origin);
-            window.close();
-          </script>
+        setTimeout(() => {
+          window.close();
+      }, 1000);
+      </script>
         `);
-        }
+    }
 
-        // Forward the code to the socket handler
-        console.log({ 'auth:google:callback': code });
-        socket.emit('auth:google:callback', code);
+    // Forward the code to the socket handler
+    console.log({ 'auth:google:callback': code });
+    socket.emit('auth:google:callback', code);
 
-        res.send(`
+    res.send(`
+        <script>
+        setTimeout(() => {
+          window.close();
+      }, 1000);
+        </script>
+        `);
+  } catch (error) {
+    logger.error(`Google callback route error: ${error.message}`);
+    console.log({ 'auth:google:callback:error': error });
+    res.status(500).send(`
         <script>
           window.close();
-        </script>
-      `);
-    } catch (error) {
-        logger.error(`Google callback route error: ${error.message}`);
-        console.log({ 'auth:google:callback:error': error });
-        res.status(500).send(`
-        <script>
           window.opener.postMessage({
             type: 'oauth-error',
             message: '${error.message.replace(/'/g, "\\'")}'
           }, window.opener.location.origin);
-          window.close();
         </script>
       `);
-    }
+  }
 });
 // Facebook OAuth callback
 router.get('/auth/facebook/callback', async (req, res) => {
-    try {
-        const { code } = req.query;
-        const socket = getIO();
+  try {
+    const { code } = req.query;
+    const socket = getIO();
 
-        if (!socket) {
-            return res.send(`
+    if (!socket) {
+      return res.send(`
               <script>
+              window.close();
                 window.opener.postMessage({
                   type: 'oauth-error',
                   message: 'Socket connection not found'
                 }, window.opener.location.origin);
-                window.close();
               </script>
             `);
-        }
+    }
 
-        // Forward the code to the socket handler
-        socket.emit('auth:facebook:callback', code);
-        console.log({ 'auth:facebook:callback': code });
+    // Forward the code to the socket handler
+    socket.emit('auth:facebook:callback', code);
+    console.log({ 'auth:facebook:callback': code });
 
-        res.send(`
-            <script>
-              window.close();
-            </script>
-          `);
-    } catch (error) {
-        logger.error(`Facebook callback route error: ${error.message}`);
-        console.log({ 'auth:facebook:callback:error': error });
-        res.status(500).send(`
+    res.send(`
+        <script>
+        setTimeout(() => {
+          window.close();
+      }, 1000);
+        </script>
+        `);
+  } catch (error) {
+    logger.error(`Facebook callback route error: ${error.message}`);
+    console.log({ 'auth:facebook:callback:error': error });
+    res.status(500).send(`
             <script>
               window.opener.postMessage({
                 type: 'oauth-error',
                 message: '${error.message.replace(/'/g, "\\'")}'
               }, window.opener.location.origin);
-              window.close();
+              setTimeout(() => {
+                window.close();
+            }, 1000);
             </script>
           `);
-    }
+  }
 });
 
 module.exports = router;
